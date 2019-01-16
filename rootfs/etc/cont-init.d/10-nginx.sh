21467a9cb363a6e190621f4787edf080377dfb7c
ludeeus <joasoe@gmail.com>
2019-01-16 03:23:46 +0100

    remove htpasswd

--- rootfs/etc/cont-init.d/10-nginx.sh
+++ rootfs/etc/cont-init.d/10-nginx.sh
@@ -21,6 +21,7 @@ if [ -z "${NO_AUTH}" ] || [ "${NO_AUTH}" == false ]; then
 
 elif [ "${NO_AUTH}" == true ]; then
   echo "NO_AUTH set to true, skipping authentication."
+  rm /etc/nginx/.htpasswd
 
 else
   echo "Something went wrong."
